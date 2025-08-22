9ca6408ab2e01b144fa664c59f4df8d343a2c2c5
Bibi Ayesha Momin <momin4930@gmail.com>
2025-08-22 08:23:51 -0700

    third  commit

---
+++ 03_Numbers.py
@@ -0,0 +1,45 @@
+# Integer (int) number type
+
+a = 1
+b = 123456789
+c = -123456789
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+
+# Float (float) number type
+
+a = 0.1
+b = 1.23456789
+c = -1.23456789
+d = 35e3
+e = 12E4
+f = -1.23e45
+
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+
+
+# Complex (complex) number type
+
+a = 3+5j
+b = 5j
+c = -5j
+d = -2j+6
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+
+
+
+
