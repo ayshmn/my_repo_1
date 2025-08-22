ab6069484bb458871e7bdbace462688e551cf5d1
Bibi Ayesha Momin <momin4930@gmail.com>
2025-08-22 02:56:40 -0700

    second commit

---
+++ 02_Comments.py
@@ -0,0 +1,29 @@
+# Methods to write comments
+
+# 1. This is a comments 
+print("Hello World")
+
+
+print("Hello World") # 2. This is a comments 
+
+
+# 3. print("Hello World")
+print("Hello World")
+
+
+# 4. This is a comments
+# written in more 
+# than just 1 line.
+print("Hello World")
+
+
+""" # 5. This a comment 
+written in more
+than just 1 line."""
+print("Hello World")
+
+
+''' # 6. This a comment 
+written in more
+than just 1 line.'''
+print("Hello World")
